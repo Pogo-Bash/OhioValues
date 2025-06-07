4ba4e4a81c9ad812539a805cb04d54565ad9485c
Swap <84603585+Pogo-Bash@users.noreply.github.com>
2025-06-06 21:03:51 -0500

    Update App.vue

--- OhioValues/src/App.vue
+++ OhioValues/src/App.vue
@@ -1,8 +1,29 @@
 <script setup>
-import index from './components/index.vue'
+import { ref } from 'vue'
+import Index from './components/index.vue'
+import ValueCalculator from './components/ValueCalculator.vue'
+
+const currentView = ref('home') // 'home' or 'calculator'
+
+const switchToHome = () => {
+  currentView.value = 'home'
+}
+
+const switchToCalculator = () => {
+  currentView.value = 'calculator'
+}
 </script>
 
 <template>
-  <index/> 
+  <div>
+    <!-- Pass navigation functions as props to both components -->
+    <Index 
+      v-if="currentView === 'home'" 
+      @switch-to-calculator="switchToCalculator"
+    />
+    <ValueCalculator 
+      v-if="currentView === 'calculator'" 
+      @switch-to-home="switchToHome"
+    />
+  </div>
 </template>
-
